8c11efb0389e050dfccecc2d2777c22bb395a824
wanganhang <2441412485@qq.com>
2023-06-26 15:24:42 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,29 @@
+print("hello world!!") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+
+"""
+锄禾日当午
+"""
+
+print(2>3)
+
+
+
+# print(type("hello world!!"))#字符串
+# print(type(2333)) #整数
+# print(type(2.333)) #小数
+# print(type(True)) #布尔值
+# print(type(())) #元组
+# print(type([])) #数组
+# print(type({})) #字典
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+a='哈哈哈哈哈'
+print(len(a))
